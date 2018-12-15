4324f350c1cdfaeffc5b236de5870a951acc3d49
Idin K <d@idin.net>
2018-12-14 17:39:16 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
       name='ravenclaw',
       version='0.1',
       description='For data wrangling.',
-      url='',
+      url='https://github.com/IdinK/ravenclaw',
       author='Idin',
       author_email='d@idin.net',
-      license='GNU AGPLv3',
+      license='MIT',
       packages=find_packages(exclude=("jupyter_tests", "examples", ".idea", ".git")),
       install_requires=['numpy', 'pandas', 'SPARQLWrapper', 'slytherin', 'gobbledegook'],
       zip_safe=False

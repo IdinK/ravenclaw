26e672c520d55a3b0c67215cf5fa4bfb5c9d906e
Idin <d@idin.net>
2018-12-14 17:37:31 -0800

    added documentation

--- ravenclaw/wrangling/long_to_wide.py
+++ ravenclaw/wrangling/long_to_wide.py
@@ -1,6 +1,14 @@
 from .standardize_columns import standardize_columns as sc
 
-def long_to_wide(data, index_by, widen_cols, value_cols, standardize_cols = True):
+def long_to_wide(data, index_by, widen_cols, value_cols, standardize_cols=True):
+	"""
+	:type data: DataFrame
+	:type index_by: list[str]
+	:type widen_cols: list[str]
+	:type value_cols: list[str]
+	:type standardize_cols: bool
+	:rtype: DataFrame
+	"""
 	result = data.pivot_table(
 		aggfunc=lambda x: x,
 		index = index_by,

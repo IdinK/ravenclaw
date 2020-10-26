e3dc927bb932911f6e7a5302531f7341094b461d
idin <py@idin.ca>
2020-10-25 17:22:27 -0700

    add get_difference and read_excel

--- ravenclaw/__init__.py
+++ ravenclaw/__init__.py
@@ -5,6 +5,8 @@ from .num_rows import num_rows, num_cols, nrows, ncols
 from .DataEnclosure import DataEnclosure
 from .get_numeric_columns import is_numeric, get_numeric_columns, get_non_numeric_columns
 from .get_numeric_columns import fill_na_for_numeric
+from .get_difference import get_difference
+from .read_excel import read_excel
 
 from .wrangling import *
 from .package_info import __build_time__

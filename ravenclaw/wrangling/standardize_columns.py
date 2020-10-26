0f6d48e9fa30623aaee719b3f59aa692b170083e
idin <py@idin.ca>
2020-10-25 17:22:06 -0700

    move text functions from slythering to txt

--- ravenclaw/wrangling/standardize_columns.py
+++ ravenclaw/wrangling/standardize_columns.py
@@ -1,16 +1,6 @@
-from slytherin import remove_non_alphanumeric
+from txt import remove_non_alphanumeric
+from txt import convert_camel_to_snake
 from pandas import DataFrame
-
-import re
-
-_first_cap_re = re.compile('(.)([A-Z][a-z]+)')
-_all_cap_re = re.compile('([a-z0-9])([A-Z])')
-def convert_camel_to_snake(string):
-	s1 = _first_cap_re.sub(r'\1_\2', string)
-	result = _all_cap_re.sub(r'\1_\2', s1).lower()
-	result = re.sub(pattern='_+', repl="_", string=result)
-	#print(manipulation, result)
-	return result
 
 
 def remove_non_alphanumeric_lower_and_join(x, camel_to_snake=True, replace_with = '_', join_by = '__', ignore_errors = False):
@@ -27,10 +17,8 @@ def remove_non_alphanumeric_lower_and_join(x, camel_to_snake=True, replace_with 
 
 	# remove empty strings and remove non alphanumeric
 	if type(x) == list:
-		#x = [remove_non_alphanumeric(s, replace_with = replace_with) for s in x if s != '']
 		x = [str(s).strip() for s in x if s != '']
 		x = join_by.join(x)
-
 
 	try:
 		x = remove_non_alphanumeric(s=x, replace_with=' ')
@@ -48,7 +36,7 @@ def remove_non_alphanumeric_lower_and_join(x, camel_to_snake=True, replace_with 
 	return x
 
 
-def standardize_columns(data, inplace = False, camel_to_snake=True):
+def standardize_columns(data, inplace=False, camel_to_snake=True):
 	"""
 	:type data: DataFrame
 	:type inplace: bool
@@ -61,7 +49,7 @@ def standardize_columns(data, inplace = False, camel_to_snake=True):
 		new_data = data.copy()
 
 	new_data.columns = list(map(
-		lambda x: remove_non_alphanumeric_lower_and_join(x = x, camel_to_snake=camel_to_snake),
+		lambda x: remove_non_alphanumeric_lower_and_join(x=x, camel_to_snake=camel_to_snake),
 		list(new_data.columns)
 	))
 
